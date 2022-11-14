3b22d5d237de02cc73ac879b93fed7c10f7bf42e
deanefraim1 <deanefraim@campus.technion.ac.il>
2022-11-14 21:27:28 +0200

    fixes to meet cpp

--- smash.cpp
+++ smash.cpp
@@ -12,21 +12,22 @@ main file. This file contains the main function of smash
 #include "commands.hpp"
 #include "signals.hpp"
 #include "Job.hpp"
+#include <iostream>
 #define MAX_LINE_SIZE 80
 #define MAXARGS 20
 
 using namespace std;
 
-char* L_Fg_Cmd;
+string L_Fg_Cmd;
 list<Job> jobs; //This represents the list of jobs. Please change to a preferred type (e.g array of char*)
-char lineSize[MAX_LINE_SIZE]; 
+string lineSize; 
 //**************************************************************************************
 // function name: main
 // Description: main function of smash. get command from user and calls command functions
 //**************************************************************************************
 int main(int argc, char *argv[])
 {
-    char cmdString[MAX_LINE_SIZE];
+    string cmdString;
 
 	//signal declaretions
 	//NOTE: the signal handlers and the function/s that sets the handler should be found in siganls.cpp
@@ -43,17 +44,12 @@ int main(int argc, char *argv[])
 
 
 	
-	L_Fg_Cmd =(char*)malloc(sizeof(char)*(MAX_LINE_SIZE+1));
-	if (L_Fg_Cmd == NULL) 
-			exit (-1); 
-	L_Fg_Cmd[0] = '\0';
 	
     	while (1)
     	{
-	 	printf("smash > ");
-		fgets(lineSize, MAX_LINE_SIZE, stdin);
-		strcpy(cmdString, lineSize);    	
-		cmdString[strlen(lineSize)-1]='\0';
+	 	cout << "smash > ";
+		fgets(lineSize, MAX_LINE_SIZE, stdin); //TODO: how to replace for strings??
+		cmdString = lineSize;
 					// perform a complicated Command
 		if(!ExeComp(lineSize)) continue; 
 					// background command	
