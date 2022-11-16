fc858da22f1760f1c20a3efd222564a347b36e3e
deanefraim1 <deanefraim@campus.technion.ac.il>
2022-11-16 20:29:50 +0200

    .

--- commands.cpp
+++ commands.cpp
@@ -139,13 +139,13 @@ int ExeComp(string lineSize)
 {
 	string ExtCmd;
 	string args[MAX_ARG];
-    if (lineSize.contains("|") 
-		 || lineSize.contains("<")
-		 || lineSize.contains(">") 
-		 || lineSize.contains("*") 
-		 || lineSize.contains("?") 
-		 || lineSize.contains(">>") 
-		 || lineSize.contains("|&"))
+    if ((lineSize.find("|") != string::npos )
+		 || (lineSize.find(">") != string::npos)
+		 || (lineSize.find("<")!= string::npos)
+		 || (lineSize.find("*") != string::npos)
+		 || (lineSize.find("?") != string::npos)
+		 || (lineSize.find(">>") != string::npos)
+		 || (lineSize.find("|&") != string::npos))
     {
 		// Add your code here (execute a complicated command)
 					
